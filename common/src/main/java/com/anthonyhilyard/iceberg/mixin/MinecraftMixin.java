8edd93f9cdccb0bf175db20e414ea78564c49888
Mat0u5 <matous.danko@gmail.com>
2026-05-21 10:58:31 +0200

    RenderTickEvents.START no longer runs multiple times per tick.

--- common/src/main/java/com/anthonyhilyard/iceberg/mixin/MinecraftMixin.java
+++ common/src/main/java/com/anthonyhilyard/iceberg/mixin/MinecraftMixin.java
@@ -13,7 +13,7 @@ import net.minecraft.client.Minecraft;
 public class MinecraftMixin
 {
 	@SuppressWarnings("resource")
-	@Inject(method = "runTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiling/ProfilerFiller;popPush(Ljava/lang/String;)V"))
+	@Inject(method = "runTick", at = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/util/profiling/ProfilerFiller;push(Ljava/lang/String;)V", args = { "ldc=gameRenderer" }))
 	public void runTick(boolean tickWorld, CallbackInfo callbackInfo)
 	{
 		Minecraft instance = (Minecraft)(Object)this;
