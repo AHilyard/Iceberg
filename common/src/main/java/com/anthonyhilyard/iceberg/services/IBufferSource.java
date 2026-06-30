cfad78caba2c4ba0b152f2cdb194d62b3eee6269
Mat0u5 <matous.danko@gmail.com>
2026-06-30 21:01:49 +0200

    Removed unused BufferSource related things.

--- common/src/main/java/com/anthonyhilyard/iceberg/services/IBufferSource.java
+++
@@ -1,9 +0,0 @@
-package com.anthonyhilyard.iceberg.services;
-
-import com.mojang.blaze3d.vertex.VertexConsumer;
-import net.minecraft.client.renderer.rendertype.RenderType;
-
-public interface IBufferSource
-{
-	VertexConsumer getBuffer(RenderType renderType);
-}
