95a36fca72a8846989b786a79b3ce18bd3483640
Mat0u5 <matous.danko@gmail.com>
2026-09-23 13:25:34 +0200

    Fixed rendering different ItemStacks in a single frame.

--- common/src/main/java/com/anthonyhilyard/iceberg/mixin/MinecraftMixin.java
+++ common/src/main/java/com/anthonyhilyard/iceberg/mixin/MinecraftMixin.java
@@ -1,5 +1,6 @@
 package com.anthonyhilyard.iceberg.mixin;
 
+import com.anthonyhilyard.iceberg.renderer.CustomItemRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,5 +19,6 @@ public class MinecraftMixin
 	{
 		Minecraft instance = (Minecraft)(Object)this;
 		RenderTickEvents.START.invoker().onStart(instance.getDeltaTracker());
+		CustomItemRenderer.onRenderTickStart();
 	}
 }
--- common/src/main/java/com/anthonyhilyard/iceberg/renderer/CustomItemRenderer.java
+++ common/src/main/java/com/anthonyhilyard/iceberg/renderer/CustomItemRenderer.java
@@ -51,6 +51,7 @@ import org.joml.Matrix4f;
 import org.joml.Matrix4fStack;
 import org.joml.Quaternionf;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.OptionalDouble;
 import java.util.OptionalInt;
@@ -76,6 +77,56 @@ public class CustomItemRenderer
 	{
 		this.minecraft = mc;
 		this.projectionMatrixBuffer = new PerspectiveProjectionMatrixBuffer("iceberg_custom_item");
+	}
+
+	private static long frameCounter = 0;
+	public static void onRenderTickStart()
+	{
+		frameCounter++;
+	}
+
+	private final List<TextureTarget> targetPool = new ArrayList<>();
+	private int maxTargetPoolCount = 10;
+	private int targetPoolIndex = 0;
+	private long lastRenderedFrame = -1;
+	private TextureTarget acquireRenderTarget(int fboWidth, int fboHeight, boolean useDepth)
+	{
+		if (frameCounter != lastRenderedFrame)
+		{
+			targetPoolIndex = 0;
+			lastRenderedFrame = frameCounter;
+		}
+
+		if (targetPoolIndex >= targetPool.size())
+		{
+			targetPool.add(getTextureTarget(fboWidth, fboHeight, useDepth));
+		}
+
+		TextureTarget target = targetPool.get(targetPoolIndex);
+		if (target.height != fboHeight || target.width != fboWidth)
+		{
+			target.destroyBuffers();
+			target = getTextureTarget(fboWidth, fboHeight, useDepth);
+			targetPool.set(targetPoolIndex, target);
+		}
+		targetPoolIndex = (targetPoolIndex + 1) % maxTargetPoolCount;
+
+		return target;
+	}
+
+	private TextureTarget getTextureTarget(int fboWidth, int fboHeight, boolean useDepth)
+	{
+		return new TextureTarget("Iceberg Item Renderer #" + targetPoolIndex, fboWidth, fboHeight, useDepth);
+	}
+
+	public int getMaxTargetPoolCount()
+	{
+		return this.maxTargetPoolCount;
+	}
+
+	public void setMaxTargetPoolCount(int count)
+	{
+		this.maxTargetPoolCount = count;
 	}
 
 	public void renderDetailModelIntoGUI(ItemStack stack, int x, int y, Quaternionf rotation, GuiGraphics graphics)
@@ -114,14 +165,7 @@ public class CustomItemRenderer
 		}
 
 		int fboSize = 96;
-		if (renderTarget == null || renderTarget.width != fboSize)
-		{
-			if (renderTarget != null)
-			{
-				renderTarget.destroyBuffers();
-			}
-			renderTarget = new TextureTarget("Iceberg Item Renderer", fboSize, fboSize, true);
-		}
+		renderTarget = acquireRenderTarget(fboSize, fboSize, true);
 
 		try (RenderPass clearPass = RenderSystem.getDevice().createCommandEncoder().createRenderPass(
 			() -> "Item",
@@ -528,11 +572,12 @@ public class CustomItemRenderer
 		}
 		isClosed = true;
 
-		if (renderTarget != null)
+		for (TextureTarget target : targetPool)
 		{
-			renderTarget.destroyBuffers();
-			renderTarget = null;
+			target.destroyBuffers();
 		}
+		targetPool.clear();
+		renderTarget = null;
 		projectionMatrixBuffer.close();
 	}
 }
